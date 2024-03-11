1aa3261e93dd4b25f2ea6ee77b736c99b89032ce
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-03-11 18:59:09 +0300

    redirect unknown routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { NextUIProvider } from '@nextui-org/react';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
 import Toast from './components/Toast/Toast';
 import AuthenticatedPageWrapper from './layouts/AuthenticatedPageWrapper';
 import PageWrapper from './layouts/PageWrapper';
@@ -23,22 +23,25 @@ function App () {
   return (
     <NextUIProvider navigate={navigate}>
       <main className='dark text-foreground bg-background min-h-screen'>
-        {!currentUser ? (
-          <Routes>
-            <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
-            <Route path="/login" element={<PageWrapper background={true}><Auth /></PageWrapper>} />
-            <Route path="/signup" element={<PageWrapper background={true}><Auth /></PageWrapper>} />
-          </Routes>
-        ): (
-          <Routes>
-            <Route path="/" element={<AuthenticatedPageWrapper><Dashboard /></AuthenticatedPageWrapper>}/>
-            <Route path="/dashboard" element={<AuthenticatedPageWrapper><Dashboard /></AuthenticatedPageWrapper>}/>
-            <Route path="/explore" element={<AuthenticatedPageWrapper>explore page</AuthenticatedPageWrapper>} />
-            <Route path="/create" element={<AuthenticatedPageWrapper>create page</AuthenticatedPageWrapper>} />
-            <Route path="/profile" element={<AuthenticatedPageWrapper>profile page</AuthenticatedPageWrapper>} />
-            <Route path="/help" element={<AuthenticatedPageWrapper>help page</AuthenticatedPageWrapper>}/>
-          </Routes>
-        )}
+        <Routes>
+          {!currentUser ? (
+            <>
+              <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
+              <Route path="/login" element={<PageWrapper background={true}><Auth /></PageWrapper>} />
+              <Route path="/signup" element={<PageWrapper background={true}><Auth /></PageWrapper>} />
+            </>
+          ): (
+            <>
+              <Route path="/" element={<AuthenticatedPageWrapper><Dashboard /></AuthenticatedPageWrapper>}/>
+              <Route path="/dashboard" element={<AuthenticatedPageWrapper><Dashboard /></AuthenticatedPageWrapper>}/>
+              <Route path="/explore" element={<AuthenticatedPageWrapper>explore page</AuthenticatedPageWrapper>} />
+              <Route path="/create" element={<AuthenticatedPageWrapper>create page</AuthenticatedPageWrapper>} />
+              <Route path="/profile" element={<AuthenticatedPageWrapper>profile page</AuthenticatedPageWrapper>} />
+              <Route path="/help" element={<AuthenticatedPageWrapper>help page</AuthenticatedPageWrapper>} />
+            </>
+          )}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
         <Toast />
       </main>
     </NextUIProvider>
