8205ff4e6fafa7aafc7c5f85b6c0b2aff65908f8
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-03-15 11:40:40 +0300

    make profile page responsive

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -10,7 +10,7 @@ const Profile = () => {
   const user = useSelector((state) => state.user)
 
   return (
-    <div className="w-full flex gap-12 px-20">
+    <div className="w-full overflow-hidden flex flex-col laptop:flex-row gap-12 mobile:px-20">
       <div className="flex flex-col">
         <Tooltip
           content="Add profile photo"
@@ -19,16 +19,16 @@ const Profile = () => {
           delay={400}
         >
           <Avatar
-            className="w-60 h-60 cursor-pointer self-center bg-background border-1 border-primary-dark shadow-medium transition hover:drop-shadow-sm"
+            className="h-[200px] w-[200px] flex items-center justify-center mobile:w-60 mobile:h-60 cursor-pointer self-center bg-background border-1 border-primary-dark shadow-medium transition hover:drop-shadow-sm"
             fallback={icons.addPhoto}
-            src= "https://avatars.githubusercontent.com/u/30373425?v=4"
+            src="https://avatars.githubusercontent.com/u/30373425?v=4"
           />
         </Tooltip>
         <H5 className="mt-6 mb-1">@{user.username}</H5>
         <P variant="tiny">{user.email}</P>
         <CustomButton className="my-6 h-10">Edit Profile</CustomButton>
       </div>
-      <Divider orientation="vertical" />
+      <Divider className="w-full h-divider laptop:h-full laptop:w-divider" />
       <div className="w-full">
         <Notes />
         <Divider className="my-8"/>
