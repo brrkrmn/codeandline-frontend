30ebc3fb5d22ac9ec036623a19d05af256354fbc
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-04-30 00:52:38 +0300

    change current note on scroll

--- frontend/src/pages/Note/Note.jsx
+++ frontend/src/pages/Note/Note.jsx
@@ -1,14 +1,15 @@
-import { BreadcrumbItem, Breadcrumbs, ScrollShadow } from "@nextui-org/react";
+import { BreadcrumbItem, Breadcrumbs, Progress, ScrollShadow } from "@nextui-org/react";
 import { useEffect, useState } from "react";
+import { InView } from "react-intersection-observer";
 import { useParams } from "react-router-dom";
 import CodeEditor from "../../components/CodeEditor/CodeEditor";
 import { editorSize } from "../../components/CodeEditor/constants";
 import TextEditor from '../../components/TextEditor';
-import useNoteTracker from "../../hooks/useNoteTracker";
 import noteService from "../../services/note";
 
 const Note = () => {
   const [note, setNote] = useState(null);
+  const [currentNote, setCurrentNote] = useState(0);
   const id = useParams().id;
 
   useEffect(() => {
@@ -23,11 +24,26 @@ const Note = () => {
     fetchNote(id);
   }, [id])
 
-  const { currentNote, handleNextNote, handlePrevNote } = useNoteTracker(note?.entries.length - 1);
+  const handleOnViewChange = (inView, entry) => {
+    if (inView) {
+      const index = note.entries.findIndex(ent => ent._id === entry.target.id)
+      setCurrentNote(index)
+    }
+  }
 
   if (note) {
     return (
       <div className="w-full flex flex-col laptop:flex-row justify-center items-start gap-4 max-h-[88vh]">
+        <Progress
+          aria-label="Scroll progress"
+          value={((currentNote + 1)/ note.entries?.length) * 100}
+          size="sm"
+          radius="none"
+          className="absolute top-[88px]"
+          classNames={{
+            indicator: "bg-gradient-to-r from-background to-[#6d4da6] to-[40%] rounded-xl"
+          }}
+        />
         <div className="w-full overflow-hidden basis-1/2">
           <Breadcrumbs
             underline="hover"
@@ -53,12 +69,17 @@ const Note = () => {
           className="basis-1/2 mt-8 px-4 w-full max-h-[80vh] flex flex-col overscroll-none snap-y snap-mandatory"
         >
           {note.entries.map((entry, index) => (
-            <TextEditor
-              key={entry.id}
-              readOnly={true}
-              value={entry.content}
+            <InView
               className="h-full mb-[70vh] py-10 snap-always snap-start"
-            />
+              key={index}
+              id={entry._id}
+              onChange={handleOnViewChange}
+            >
+              <TextEditor
+                readOnly={true}
+                value={entry.content}
+              />
+            </InView>
           ))}
         </ScrollShadow>
       </div>
