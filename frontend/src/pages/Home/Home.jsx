abaffef9b5d2c1ce9a44c405ab7173694278c0e9
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-05-08 16:04:52 +0300

    add radial background to landing

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,18 +1,35 @@
-import { useInView } from 'react-intersection-observer';
+import { motion, useScroll, useTransform } from 'framer-motion';
+import { useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import icons from '../../assets/icons';
 import Video from '../../assets/videos/landing.mp4';
 import CustomButton from '../../components/CustomButton';
-import { H1, P } from '../../components/Typography';
+import { H1 } from '../../components/Typography';
+import { pStyles } from '../../components/Typography/constants';
 import VideoPlayer from '../../components/VideoPlayer/VideoPlayer';
 import FolderSection from './FolderSection';
 import ToolbarSection from './ToolbarSection';
 
 const Home = () => {
   const navigate = useNavigate();
-  const [ videoRef, videoInView ] = useInView({
-    threshold: 0.7
+  const videoRef = useRef();
+  const lastSectionRef = useRef();
+  const { scrollYProgress: videoScrollYProgress } = useScroll({
+    target: videoRef,
+    offset: ["start center", "end start"]
   })
+
+  const { scrollYProgress: lastSectionScrollYProgress } = useScroll({
+    target: lastSectionRef,
+    offset: ["start end", "end start"]
+  })
+
+  const opacity = useTransform(videoScrollYProgress, [0, 0.3, 0.8, 1], [0.6, 1, 1, 0])
+  const shadow = useTransform(videoScrollYProgress, [0, 0.5, 1], ["0 0 70px 0 rgba(191,151,255,0.44)", "0 -5px 70px 0 rgba(191,151,255,0.44)", "0 0 100px 0 rgba(191,151,255,1)"])
+  const background = useTransform(lastSectionScrollYProgress, [0, 0.5], [
+    "radial-gradient(circle, rgba(3,0,20,1) 2%, rgba(191,151,255,0.14) 4%, rgba(3,0,20,1) 10%, rgba(191,151,255,0.14) 20%, rgba(3,0,20,1) 30%, rgba(191,151,255,0.14) 40%, rgba(3,0,20,1) 50%)",
+    "radial-gradient(circle, rgba(3,0,20,1) 10%, rgba(191,151,255,0.14) 40%, rgba(3,0,20,1) 50%, rgba(191,151,255,0.14) 50%, rgba(3,0,20,1) 60%, rgba(191,151,255,0.14) 60%, rgba(3,0,20,1) 70%)"
+  ])
 
   return (
     <div
@@ -32,25 +49,47 @@ const Home = () => {
           Start Now {icons.chevronRight}
         </CustomButton>
       </div>
-      <div
+      <motion.div
+        style={{ opacity }}
         ref={videoRef}
         className="z-30 flex flex-col items-center justify-center"
       >
-        <VideoPlayer
-          src={Video}
-          enlargeOnView={true}
-          className={`customPerspective self-center w-[80%] border-1 animate-[slideInFromTop_1.5s_ease-in-out] transition-all duration-1000 delay-300 border-primary-dark shadow-[0_-5px_70px_0_rgba(191,151,255,0.44)] rounded-lg *:rounded-lg flex items-center justify-center`}
-        />
-        <P
-          variant="big"
-          className={`transition delay-700 duration-1000 pt-10 -translate-y-20 opacity-0 ${videoInView && 'translate-y-0 opacity-100'}`}
+        <motion.div
+          style={{ boxShadow: shadow }}
+          className="self-center w-[80%] border-1 animate-[slideInFromTop_1.5s_ease-in-out] transition-all duration-1000 delay-300 border-primary-dark rounded-lg *:rounded-lg flex items-center justify-center"
+        >
+          <VideoPlayer
+            src={Video}
+            enlargeOnView={true}
+            className="w-full h-full"
+          />
+        </motion.div>
+        <motion.p
+          className={`${pStyles.big} pt-10`}
         >
           Comprehensive and detailed code documentation
-        </P>
-      </div>
+        </motion.p>
+      </motion.div>
       <FolderSection />
       <ToolbarSection />
-      <div className="h-screen"></div>
+      <motion.div
+        ref={lastSectionRef}
+        style={{ backgroundImage: background }}
+        className="bg-contain h-[1200px] w-full self-center h-screen flex flex-col items-center justify-center gap-4"
+      >
+        <H1 className="text-6xl text-center text-transparent tracking-wide bg-clip-text bg-gradient-to-br from-foreground-dark to-[#F7F8F8]">
+          Line-by-line Clarity
+        </H1>
+        <p className="text-xl font-medium text-foreground-dark">
+          Gain full understanding of your code
+        </p>
+        <CustomButton
+          onPress={() => navigate('/signup')}
+          className="mt-4 animate-[gradient_5s_ease_infinite] transition *:hover:translate-x-4 min-h-10 border-primary-dark text-primary-light gradientBackground2"
+        >
+          Sign up for free{icons.chevronRight}
+        </CustomButton>
+      </motion.div>
     </div>
   )
 };
--- frontend/src/pages/Home/ToolbarSection.jsx
+++ frontend/src/pages/Home/ToolbarSection.jsx
@@ -16,9 +16,8 @@ const ToolbarSection = () => {
   const scale2 = useTransform(scrollYProgress, [0, 0.5], [0.8, 1.1]);
   const yDown = useTransform(scrollYProgress, [0, 0.5], ["0%", "150%"]);
   const yDown2 = useTransform(scrollYProgress, [0, 0.5], ["0%", "50%"])
-  const yUp = useTransform(scrollYProgress, [0, 0.4], ["100%", "-10%"]);
+  const yUp = useTransform(scrollYProgress, [0, 0.4, 0.6], ["100%", "-10%", "-100"]);
   const shadow = useTransform(scrollYProgress, [0, 0.5], ["0 0 1px #bf97ff70", "0 2px 20px #bf97ff"])
-  const xRight = useTransform(scrollYProgress, [0.1, 0.4], ["200%", "0%"]);
 
   return (
     <motion.div
@@ -49,23 +48,13 @@ const ToolbarSection = () => {
         />
       </motion.div>
       <motion.div
-        style={{ opacity: noteOpacity, x: xRight }}
-        className="w-[600px] h-full absolute -right-[480px] top-[200px]"
-      >
-        <TextEditor
-          readOnly
-          className="z-1 px-8 py-4 scale-[.80] border-1 border-divider rounded-xl bg-gradient-to-b from-background to-border bg-clip-padding"
-          value={`<blockquote>useTransform creates a MotionValue that takes the output of one or more other MotionValues and changes it some way. </blockquote><p><br></p><p><span style="color: rgb(198, 120, 221);">const</span> backgroundColor <span style="color: rgb(86, 182, 194);">=</span> <span style="color: rgb(97, 175, 239);">useTransform</span>(</p><p>    scrollYProgress,</p><p>    [<span style="color: rgb(209, 154, 102);">0</span>, <span style="color: rgb(209, 154, 102);">0.6</span>],</p><p>    [<span style="color: rgb(152, 195, 121);">"radial-gradient...</span><span style="background-color: hsl(var(--nextui-background) / var(--nextui-background-opacity, var(--tw-bg-opacity))); color: rgb(152, 195, 121);">"</span><span style="background-color: hsl(var(--nextui-background) / var(--nextui-background-opacity, var(--tw-bg-opacity)));">, </span><span style="color: rgb(152, 195, 121);">"radial-...</span><span style="background-color: hsl(var(--nextui-background) / var(--nextui-background-opacity, var(--tw-bg-opacity))); color: rgb(152, 195, 121);">"</span><span style="background-color: hsl(var(--nextui-background) / var(--nextui-background-opacity, var(--tw-bg-opacity)));">]</span></p><p>);</p>`}
-        />
-      </motion.div>
-      <motion.div
         style={{ opacity: noteOpacity, y: yUp }}
-        className="w-[600px] h-full absolute -left-[350px] top-[300px]"
+        className="w-[600px] h-full mt-40 flex items-center justify-start"
       >
         <TextEditor
           readOnly
-          className="px-8 py-4 scale-[.80] border-1 border-divider rounded-xl bg-gradient-to-b from-background to-border bg-clip-padding"
-          value={`<h1><span style="color: rgb(194, 133, 255);">Scroll Animations</span> with Framer Motion</h1><p><br></p><p>The code utilizes several features from <strong style="color: rgb(102, 163, 224); background-color: rgb(0, 41, 102);">Framer Motion</strong> to create dynamic animations based on the user's scroll behavior.</p><p><br></p><blockquote><span style="background-color: rgb(68, 68, 68); color: rgb(255, 255, 255);">useScroll</span> is used to create scroll-linked animations, like progress indicators and parallax effects.</blockquote><p><br></p><p>Firstly, it employs the <span style="background-color: rgb(68, 68, 68); color: rgb(255, 255, 255);">useScroll</span> hook to track the scroll position of a specific reference element, sectionRef. This hook returns a <span style="color: rgb(255, 255, 255); background-color: rgb(68, 68, 68);">scrollYProgress</span> value, indicating the progress of scrolling from <u>0 to 1</u>. </p><p><br></p><p>Next, the useTransform hook is used to map the scrollYProgress value to various output ranges, enabling animations based on the scroll position. It alters </p><ul><li>opacity, </li><li>scale,</li><li>translate (xRight, xRight2, xLeft)</li><li><span style="color: rgba(239, 237, 253, 0.7);">backgroundColor</span></li></ul><p><br></p><p><br></p>`}
+          className="px-8 py-4 scale-[.80] border-1 border-divider rounded-xl bg-gradient-to-b from-background to-divider bg-clip-padding"
+          value={`<h1><span style="color: rgb(194, 133, 255);">Scroll Animations</span> with Framer Motion</h1><p><br></p><p>The code utilizes several features from <strong style="color: rgb(255, 225, 0); background-color: rgba(243, 238, 255, 0.06);">Framer Motion</strong> to create dynamic animations based on the user's scroll behavior.</p><p><br></p><blockquote><span style="background-color: rgb(68, 68, 68); color: rgb(255, 255, 255);">useScroll</span> is used to create scroll-linked animations, like progress indicators and parallax effects.</blockquote><p><br></p><p>Firstly, it employs the <span style="background-color: rgb(68, 68, 68); color: rgb(255, 255, 255);">useScroll</span> hook to track the scroll position of a specific reference element, sectionRef. This hook returns a <span style="color: rgb(255, 255, 255); background-color: rgb(68, 68, 68);">scrollYProgress</span> value, indicating the progress of scrolling from <u>0 to 1</u>. </p><p><br></p><p>Next, the useTransform hook is used to map the scrollYProgress value to various output ranges, enabling animations based on the scroll position. It alters </p><ul><li>opacity, </li><li>scale,</li><li>translate (xRight, xRight2, xLeft)</li><li><span style="color: rgba(239, 237, 253, 0.7);">backgroundColor</span></li></ul><p><br></p><p><br></p>`}
         />
       </motion.div>
     </motion.div>
