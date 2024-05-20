0ca9e77628966e86e92729b12e3d717f00a9e18c
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-05-20 16:06:43 +0300

    edit footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,13 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import icons from '../../assets/icons';
 import Logo from '../Logo/Logo';
+import { P } from '../Typography';
 
 const Footer = () => {
   return (
-    <div className="py-20 px-4 tablet:px-40 wide:px-96 border-t-1 border-divider flex gap-8">
+    <div className="py-20 px-4 tablet:px-40 wide:px-96 border-t-1 border-divider flex gap-8 items-center">
       <Logo className="mr-auto" />
-      <Link to={'https://github.com/brrkrmn/codeandline'}>{icons.github}</Link>
-      {/* <Link>{icons.mail}</Link> */}
+      <div className="flex items-center justify-center gap-2">
+        <P variant="tiny" className="font-normal">made by</P>
+        <Link to="https://www.linkedin.com/in/berra-karaman-3936471b0/">
+          <P variant="tiny" className="font-normal transition hover:text-primary-light">Berra Karaman</P>
+        </Link>
+      </div>
+      <Link
+        to={'https://github.com/brrkrmn/codeandline'}
+        className="*:transition *:hover:text-primary-light"
+      >
+        {icons.github}
+      </Link>
     </div>
   )
 }
