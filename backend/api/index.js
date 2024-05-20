39ad677a84f7acedbcc29f670b79c4d6e1ba779e
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-05-20 13:22:32 +0300

    use vercel.json

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,5 @@
-const app = require('./app');
-const config = require('./utils/config');
+const app = require('../app');
+const config = require('../utils/config');
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
