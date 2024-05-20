68d91a936f95f7aeada0f622ccd5f799e241c6ba
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-05-20 14:37:33 +0300

    bring back cors

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const signupRouter = require('./controllers/signup');
 const notesRouter = require('./controllers/notes')
 const middleware = require('./utils/middleware');
 const foldersRouter = require('./controllers/folders');
+const cors = require('cors');
 
 mongoose.set('strictQuery', false);
 console.log('Connecting to MONGODB...');
@@ -23,6 +24,7 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(express.static('build'))
 app.use(express.json())
+app.use(cors());
 
 app.use(middleware.requestLogger)
 
