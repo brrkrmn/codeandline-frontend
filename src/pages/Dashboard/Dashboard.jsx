7837d8b2709b672b0ece8ea262fc5d4438e9e96c
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-05-22 10:54:16 +0300

    add loading state to dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { Divider, Spacer } from "@nextui-org/react"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import CardSection from "../../components/CardSection/CardSection"
 import { cardSectionTypes } from "../../components/CardSection/constants"
@@ -10,29 +10,37 @@ import { getUserFolders } from "../../reducers/foldersReducer"
 import { getUserNotes } from "../../reducers/notesReducer"
 
 const Dashboard = () => {
+  const [loading, setLoading] = useState(true)
   const dispatch = useDispatch();
   const path = window.location.pathname.split('/')[1]
   const notes = useSelector((state) => state.notes)
   const folders = useSelector((state) => state.folders)
 
   useEffect(() => {
-    dispatch(getUserNotes())
-    dispatch(getUserFolders())
+    const fetchData = async () => {
+      await dispatch(getUserNotes())
+      await dispatch(getUserFolders())
+      setLoading(false)
+    }
+    fetchData()
   }, [])
 
-  return (
-    <div className="w-full laptop:px-20 flex flex-col tablet:flex-row justify-start items-start gap-4">
-      <MenuList />
-      {notes.length === 0 && folders.length === 0 ? null : (
-        <Spacer
-          className="tablet:w-[300px] tablet:min-w-[300px]"
-        />
-      )}
-      <div className="w-full overflow-hidden flex flex-col gap-10">
-        {path === 'folder-overview' ? (
+  if (loading) {
+    return null
+  } else {
+    return (
+      <div className="w-full laptop:px-20 flex flex-col tablet:flex-row justify-start items-start gap-4">
+        <MenuList />
+        {notes.length === 0 && folders.length === 0 ? null : (
+          <Spacer
+            className="tablet:w-[300px] tablet:min-w-[300px]"
+          />
+        )}
+        <div className="w-full overflow-hidden flex flex-col gap-10">
+          {path === 'folder-overview' ? (
             <FolderOverview />
-        ) : path === 'note-overview' ? (
-          <NoteOverview />
+          ) : path === 'note-overview' ? (
+            <NoteOverview />
           ) : (
             <>
               <CardSection type={cardSectionTypes.NOTES} />
@@ -40,10 +48,11 @@ const Dashboard = () => {
               <CardSection type={cardSectionTypes.FOLDERS} noTitle={true} />
             </>
           )
-        }
+          }
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Dashboard
