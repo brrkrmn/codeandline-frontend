054c143499cf3395415519d5617c01bb9b7f791f
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-05-29 17:41:09 +0300

    use debounce in quill

--- src/components/TextEditor/TextEditor.js
+++ src/components/TextEditor/TextEditor.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import { debounce } from 'lodash';
+import React, { useRef } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.bubble.css';
 import 'react-quill/dist/quill.snow.css';
 
 const TextEditor = ({ name, value, onChange, readOnly = false, className, snow = false }) => {
+  const quillRef = useRef(null);
+
+  const saveCursorPosition = () => {
+    const quill = quillRef.current.getEditor();
+    const selection = quill.getSelection();
+    return selection ? selection.index : null;
+  }
+
+  const restoreCursorPosition = (position) => {
+    if (position !== null) {
+      const quill = quillRef.current.getEditor();
+      quill.setSelection(position)
+    }
+  }
+
+  const handleTextChange = debounce((value) => {
+    const position = saveCursorPosition();
+    onChange(value);
+    restoreCursorPosition(position);
+  }, 300)
+
   return (
     <ReactQuill
+      ref={quillRef}
       theme={snow ? 'snow' : 'bubble'}
       name={name}
       value={value}
-      onChange={onChange}
+      onChange={handleTextChange}
       readOnly={readOnly}
       placeholder='Write something ... Select text to access to the toolbar.'
       className={`rounded-lg ${className}`}
